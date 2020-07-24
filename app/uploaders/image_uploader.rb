e73f90e56e4ea16c5ee5786f9c5c247be087fa7d
taiga0307 <tai03.ga07@gmail.com>
2020-07-24 16:57:37 +0900

    [add]s3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -35,12 +41,18 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
+  def extension_whitelist
+    %w(png jpg)
+  end
   # def extension_whitelist
   #   %w(jpg jpeg gif png)
   # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  def filename
+    original_filename if original_filename
+  end
   # def filename
   #   "something.jpg" if original_filename
   # end
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,26 @@
+# developmentの設定
+if Rails.env.development?
+  CarrierWave.configure do |config|
+    config.cache_storage = :file
+  end
+# testの設定
+elsif Rails.env.test?
+  CarrierWave.configure do |config|
+    config.cache_storage = :file
+  end
+# productionの設定
+else
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_DEFAULT_REGION']
+    }
+
+    config.fog_directory = ENV['AWS_BUCKET']
+    config.cache_storage = :fog
+    # S3のURLに直アクセス禁止
+    config.fog_public = false
+  end
+end
