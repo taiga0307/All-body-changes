812ecd1998795e208121e7cade8cb69b5ce834b2
taiga0307 <tai03.ga07@gmail.com>
2020-08-01 15:38:00 +0900

    [add]validates

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -5,6 +5,23 @@ class Gym < ApplicationRecord
   belongs_to :genre
   belongs_to :admin_user, optional: true # 外部キーは必ず紐づかないといけないため外部キーがnillでも大丈夫。1対1
 
+  validates :gym_brand, presence: true, length: { maximum:15 } # 文字数最大15文字
+  validates :gym_name, presence: true, length: { maximum:30 } # 文字数最大30文字
+  validates :gym_description, presence: true, length: { maximum:800 } # 文字数最大800文字
+  validates :gym_picture_id, presence: true
+
+  validates :gym_tell, presence: true
+  validates :gym_postal_code, presence: true
+  validates :gym_address, presence: true
+  validates :prefecture_code, presence: true
+  validates :address_city, presence: true
+  validates :address_street, presence: true
+  validates :gym_valid, presence: true
+
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+
+
   mount_uploader :gym_picture_id, ImageUploader #activeadminにてアップロードするため記述
 
 
